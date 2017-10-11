ec93f0739586eb479e56799ab7b30d557742a47e
Levi Zitting <levizitting5663@gmail.com>
2017-10-11 13:07:55 -0500

    Nested components

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 Vue.component('task-list', {
-	template: '<ul><slot></slot></ul>'
+	template: `<ul>
+	<task v-for="task in tasks">{{ task.task }}</task>
+</ul>`,
+	
+	data() {
+		return {
+			tasks: [
+				{ task: 'Go to the store', complete: true },
+				{ task: 'Go to the bank', complete: true },
+				{ task: 'Go to work', complete: true },
+				{ task: 'Go to the movies', complete: true },
+			]
+		}
+	}
 });
 
 Vue.component('task', {
