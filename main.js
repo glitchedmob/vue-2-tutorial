dd8a9f5fb449668a1075a33c61f9fdc721d1cbc9
Levi Zitting <levizitting5663@gmail.com>
2017-10-12 12:37:07 -0500

    Vue event dispatcher with custom wrapper

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+// Shared event instance
+window.Event = new class {
+	constructor() {
+		this.vue = new Vue();
+	}
+
+	fire(event, data = null) {
+		this.vue.$emit(event, data);
+	}
+
+	listen(event, callback) {
+		this.vue.$on(event, callback);
+	}
+}
+
+
 Vue.component('coupon', {
 	template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
 
 	methods: {
 		onCouponApplied() {
-			this.$emit('applied');
+			Event.fire('applied');
 		}
 	}
 })
@@ -16,10 +32,11 @@ new Vue({
 		couponApplied: false
 	},
 
-	methods: {
-		onCouponApplied() {
+
+	created() {
+		Event.listen('applied', () => {
 			this.couponApplied = true;
-		}
+		})
 	}
 })
 
