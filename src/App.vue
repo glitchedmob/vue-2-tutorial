42e84f91387fe83d2480db5340e9d07a3fe79aff
Levi Zitting <levizitting5663@gmail.com>
2017-10-13 10:21:15 -0500

    Hot module replacement quick example

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
   <div id="app">
-    <message>Hello there</message>
-    <message>Hello there</message>
+    <counter></counter>
   </div>
 </template>
 
 <script>
 
-import Message from './components/Message.vue';
+import Counter from './components/Counter.vue';
 
 export default {
   name: 'app',
 
-  components: { Message },
-
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  components: { Counter },
 }
 </script>
 
