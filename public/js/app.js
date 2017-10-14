166eea9baeb271004f4d5cbb42da619d5a8141e3
Levi Zitting <levizitting5663@gmail.com>
2017-10-14 00:00:53 -0500

    OOP forms part 3

--- public/js/app.js
+++ public/js/app.js
@@ -1,57 +1,107 @@
-class Errors {
+class Errors 
+{
 
-	constructor() {
+	constructor() 
+	{
 		this.errors = {};
 	}
 
-	has(field) {
+	has(field) 
+	{
 		return this.errors.hasOwnProperty(field);
 	}
 
-	any() {
+	any() 
+	{
 		return Object.keys(this.errors).length > 0;
 	}
 
-	get(field) {
+	get(field) 
+	{
 		if(this.has(field)) {
 			return this.errors[field][0];
 		}
 	}
 
-	record(errors) {
+	record(errors) 
+	{
 		this.errors = errors;
 	}
 
-	clear(field) {
-		delete this.errors[field];
+	clear(field) 
+	{
+		if (field) { 
+			delete this.errors[field]
+		} else {
+			this.errors = {};
+		}
 	}
 
+}
+
+class Form 
+{
+	constructor(data) 
+	{
+		this.originalData = data;
+
+		for(let field in data) {
+			this[field] = data[field];
+		}
+
+		this.errors = new Errors();
+	}
+
+	data()
+	{
+		let data = Object.assign({}, this);
+		delete data.originalData;
+		delete data.errors;
+
+		return data;
+	}
+
+	reset() 
+	{
+		for (let field in this.originalData) {
+			this.field = '';
+		}
+	}
+
+	submit(requestType, url)
+	{
+			axios[requestType.toLowerCase()](url, this.data())
+				.then(this.onSuccess.bind(this))
+				.catch(this.onFail.bind(this));
+	}
+
+	onSuccess(response)
+	{
+		alert(response.data.message);
+
+		this.errors.clear();
+		this.reset();
+	}
+
+	onFail(error)
+	{
+		this.errors.record(error.response.data.errors)
+	}
 }
 
 new Vue({
 	el: '#app',
 
 	data: {
-		name: '',
-		description: '',
-		errors: new Errors()
+		form: new Form({
+			name: '',
+			description: '',
+		}),
 	},
 
 	methods: {
 		onSubmit() {
-			axios.post('/projects', {
-				name: this.name,
-				description: this.description
-			})
-				.then(this.onSuccess)
-				.catch(error =>	{this.errors.record(error.response.data.errors)});
-		},
-
-		onSuccess(response) {
-			alert(response);
-	
-			this.name = '';
-			this.description = '';
+			this.form.submit('POST', '/projects');
 		}
 	}
 });
