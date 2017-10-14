18dced3d1d58fd74b20284f59102bbf4160e7c73
Levi Zitting <levizitting5663@gmail.com>
2017-10-14 09:00:43 -0500

    OOP forms part 3

--- public/js/app.js
+++ public/js/app.js
@@ -6,16 +6,27 @@ class Errors
 		this.errors = {};
 	}
 
+	/**
+	 * Checks if a paticular error exists
+	 * @param {string} field 
+	 */
 	has(field) 
 	{
 		return this.errors.hasOwnProperty(field);
 	}
 
+	/**
+	 * Checks if there are any errors
+	 */
 	any() 
 	{
 		return Object.keys(this.errors).length > 0;
 	}
 
+	/**
+	 * Retrieves the error message for a given error
+	 * @param {string} field 
+	 */
 	get(field) 
 	{
 		if(this.has(field)) {
@@ -23,11 +34,19 @@ class Errors
 		}
 	}
 
+	/**
+	 * Records an error
+	 * @param {*} errors 
+	 */
 	record(errors) 
 	{
 		this.errors = errors;
 	}
 
+	/**
+	 * Clears a paticular error, or all errors if there is no field argument
+	 * @param {string} field 
+	 */
 	clear(field) 
 	{
 		if (field) { 
@@ -52,40 +71,72 @@ class Form
 		this.errors = new Errors();
 	}
 
+	/**
+	 * Returns the data from the form fields
+	 */
 	data()
 	{
-		let data = Object.assign({}, this);
-		delete data.originalData;
-		delete data.errors;
+		let data = {};
+
+		for (let property in this.originalData) {
+			data[property] = this[property];
+		}
 
 		return data;
 	}
 
+	/**
+	 * Resets all form fields
+	 */
 	reset() 
 	{
 		for (let field in this.originalData) {
 			this.field = '';
 		}
-	}
-
-	submit(requestType, url)
-	{
-			axios[requestType.toLowerCase()](url, this.data())
-				.then(this.onSuccess.bind(this))
-				.catch(this.onFail.bind(this));
-	}
-
-	onSuccess(response)
-	{
-		alert(response.data.message);
 
 		this.errors.clear();
+	}
+
+	/**
+	 * Submits the form to a specified end point using AJAX
+	 * @param {string} requestType 
+	 * @param {string} url 
+	 */
+	submit(requestType, url)
+	{
+		return new Promise((resolve, reject) => {
+			axios[requestType.toLowerCase()](url, this.data())
+				.then(response => {
+					this.onSuccess(response.data).bind(this);
+					resolve(response.data);
+				})
+				.catch(error => {
+					this.onFail(error.response.data.errors).bind(this);
+					reject(error.response.data.errors);
+				});
+		});
+		
+			
+	}
+
+	/**
+	 * Runs if the form submit was successful
+	 * Clears all errors and resets the form
+	 * @param {*} data 
+	 */
+	onSuccess(data)
+	{
 		this.reset();
 	}
 
-	onFail(error)
+	/**
+	 * Runs if the form submit was unsuccessful
+	 * records all errors
+	 * @param {*} errors 
+	 */
+	onFail(errors)
 	{
-		this.errors.record(error.response.data.errors)
+		this.errors.record(errors)
 	}
 }
 
@@ -101,7 +152,9 @@ new Vue({
 
 	methods: {
 		onSubmit() {
-			this.form.submit('POST', '/projects');
+			this.form.submit('POST', '/projects')
+				.then(data => alert(data))
+				.catch(errors => console.log(errors));
 		}
 	}
 });
